47cf88229f0f08488dc8b9e939916d01a48c4496
kaycee <se.kaycee@gmail.com>
2022-08-12 22:52:18 +0100

    Add dynamic section transitions

--- front/scripts/start.js
+++ front/scripts/start.js
@@ -1,17 +1,38 @@
 const sections = document.querySelectorAll('.section')
 const control = document.querySelectorAll('.navbar')
 const btns = document.querySelectorAll('.nav')
-const body = document.querySelectorAll('.main-content')
+const body = document.querySelector('.main-content')
 
 function PageTransitions() {
   // event handler for buttons in the navbar
   for (let i = 0; i < btns.length; i++) {
     btns[i].addEventListener('click', function() {
-      let currentBtn = document.querySelectorAll('.btn-active')
-      currentBtn[0].className = currentBtn[0].className.replace('btn-active', '')
-      this.className += ' btn-active'
+      let activeBtn = document.querySelectorAll('.btn-active')
+      activeBtn[0].className = activeBtn[0].className.replace('btn-active', '').trim()
+      this.className += ' btn-active'   
     })
   }
+
+  // event handler for sections in the body
+  body.addEventListener('click', e => {
+    console.log(e.target)
+    const id = e.target.dataset.id
+    if (id) {
+      // remove 'active' class from the other buttons
+      control.forEach((btn) => {
+        btn.classList.remove('active')
+      })
+      e.target.classList.add('active')
+
+      // hide other sections
+      sections.forEach((section) => {
+        section.classList.remove('active')
+      })
+
+      const el = document.getElementById(id)
+      el.classList.add('active')
+    }
+  })
 }
 
 PageTransitions()
